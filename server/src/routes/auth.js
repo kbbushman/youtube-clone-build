a30ec88ff7b1683fd6000385801838d24448301b
kbbushman <k.bushman@yahoo.com>
2021-01-27 23:05:54 -0800

    Adds login route and controller

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,14 +1,43 @@
+import { PrismaClient } from '@prisma/client';
 import express from "express";
-// A function to get the routes.
-// All route definitions are in one place and we only need to export one thing
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
 function getAuthRoutes() {
   const router = express.Router();
+
+  router.post('/google-login', googleLogin);
 
   return router;
 }
 
-// All controllers/utility functions here
-async function googleLogin(req, res) {}
+async function googleLogin(req, res) {
+  const { username , email } = req.body;
+
+  let user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    user = await prisma.user.create({
+      data: {
+        username,
+        email,
+      },
+    });
+  }
+
+  const tokenPayload = { id: user.id };
+  const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+
+  res.cookie('token', token, { httpOnly: true });
+  res.status(200).send(token);
+}
 
 async function me(req, res) {}
 
