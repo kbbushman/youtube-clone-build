da05cf29db385fbe1b0136aeeec6b842466f0545
kbbushman <k.bushman@yahoo.com>
2021-01-27 23:25:26 -0800

    Adds protect method with token validation

--- server/src/middleware/authorization.js
+++ server/src/middleware/authorization.js
@@ -1,3 +1,37 @@
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export async function getAuthUser(req, res, next) {}
 
-export async function protect(req, res, next) {}
+export async function protect(req, res, next) {
+  if (!req.headers.authorization) {
+    return next({
+      message: 'You must be logged in to visit this route',
+      statusCode: 401,
+    });
+  }
+
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+      include: {
+        videos: true,
+      }
+    });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return next({
+      message: 'You must be logged in to visit this route',
+      statusCode: 401,
+    });
+  }
+}
