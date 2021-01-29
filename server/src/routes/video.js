1942abc1ce2d8189e7ef7d3488e942742e590beb
kbbushman <k.bushman@yahoo.com>
2021-01-28 23:39:05 -0800

    Adds create video route and controller

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client';
+import { protect } from '../middleware/authorization';
 
 const prisma = new PrismaClient();
 
@@ -9,6 +10,8 @@ function getVideoRoutes() {
   router.get('/', getRecommendedVideos);
   router.get('/trending', getTrendingVideos);
   router.get('/search', searchVideos);
+
+  router.post('/', protect, addVideo);
 
   return router;
 }
@@ -108,7 +111,25 @@ async function searchVideos(req, res, next) {
   res.status(200).json({ videos });
 }
 
-async function addVideo(req, res) {}
+async function addVideo(req, res) {
+  const { title, description, url, thumbnail } = req.body;
+
+  const video = await prisma.video.create({
+    data: {
+      title,
+      description,
+      url,
+      thumbnail,
+      user: {
+        connect: {
+          id: req.user.id,
+        },
+      },
+    },
+  });
+
+  res.status(200).json({ video });
+}
 
 async function addComment(req, res, next) {}
 
