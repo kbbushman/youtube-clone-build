dec509e012d75a4e621d689d8e9c3d8f52c90b49
kbbushman <k.bushman@yahoo.com>
2021-01-28 18:57:59 -0800

    Adds trending videos route and controller

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -7,6 +7,8 @@ function getVideoRoutes() {
   const router = express.Router();
 
   router.get('/', getRecommendedVideos);
+  router.get('/trending', getTrendingVideos);
+
 
   return router;
 }
@@ -46,7 +48,26 @@ async function getRecommendedVideos(req, res) {
   res.status(200).json({ videos });
 }
 
-async function getTrendingVideos(req, res) {}
+async function getTrendingVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  
+  videos = await getVideoViews(videos);
+  videos.sort((a, b) => b.views - a.views);
+
+  res.status(200).json({ videos });
+}
 
 async function searchVideos(req, res, next) {}
 
