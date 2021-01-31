979f26945d720117a4b169a8615ba5c1e9c58ff1
kbbushman <k.bushman@yahoo.com>
2021-01-30 23:24:19 -0800

    Adds search users route and controller

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client';
-import { protect } from "../middleware/authorization";
+import { getAuthUser, protect } from "../middleware/authorization";
 import { getVideoViews } from './video';
 
 const prisma = new PrismaClient();
@@ -12,6 +12,7 @@ function getUserRoutes() {
   router.get('/history', protect, getHistory);
   router.get('/:userId/toggle-subscribe', protect, toggleSubscribe);
   router.get('/subscriptions', protect, getFeed);
+  router.get('/search', getAuthUser, searchUser);
 
   return router;
 }
@@ -148,7 +149,69 @@ async function getFeed(req, res) {
   return res.status(200).json({ feed: feedVideos });
 }
 
-async function searchUser(req, res, next) {}
+async function searchUser(req, res, next) {
+  if (!req.query.query) {
+    return next({
+      message: 'Please enter a search query',
+      statusCode: 400,
+    });
+  }
+
+  const users = await prisma.user.findMany({
+    where: {
+      username: {
+        contains: req.query.query,
+        mode: 'insensitive',
+      }
+    }
+  });
+
+  if (!users.length) {
+    return res.status(200).json({ users });
+  }
+
+  for (const user of users) {
+    const subscribersCount = await prisma.subscription.count({
+      where: {
+        subscribedToId: {
+          equals: user.id,
+        }
+      }
+    });
+
+    const videosCount = await prisma.video.count({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    let isMe = false;
+    let isSubscribed = false;
+
+    if (req.user) {
+      isMe = req.user.id === user.id;
+      isSubscribed = await prisma.subscription.findFirst({
+        where: {
+          AND: {
+            subscriberId: {
+              equals: req.user.id,
+            },
+            subscribedToId: {
+              equals: user.id,
+            },
+          },
+        },
+      });
+    }
+
+    user.subcribersCount = subscribersCount;
+    user.videosCount = videosCount;
+    user.isSubscribed = Boolean(isSubscribed);
+    user.isMe = isMe;
+  }
+
+  res.status(200).json({ users });
+}
 
 async function getRecommendedChannels(req, res) {}
 
