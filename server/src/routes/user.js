0d4396b1db1e4390a0042bd335ecec45fc1006dd
kbbushman <k.bushman@yahoo.com>
2021-01-30 22:14:43 -0800

    Adds user liked videos route and controller

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,12 +1,49 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client';
+import { protect } from "../middleware/authorization";
+import { getVideoViews } from './video';
+
+const prisma = new PrismaClient();
 
 function getUserRoutes() {
   const router = express.Router();
 
+  router.get('/liked-videos', protect, getLikedVideos);
+
   return router;
 }
 
-async function getLikedVideos(req, res, next) {}
+async function getLikedVideos(req, res, next) {
+  const videoLikes = await prisma.videoLike.findMany({
+    where: {
+      userId: req.user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  const videoIds = videoLikes.map((videoLike) => videoLike.videoId);
+
+  let videos = await prisma.video.findMany({
+    where: {
+      id: {
+        in: videoIds
+      },
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  videos =  await getVideoViews(videos);
+
+  return res.status(200).json({ videos });
+}
 
 async function getHistory(req, res, next) {}
 
