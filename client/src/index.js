8a5114b48981152f73ef50c0f5dbd9e0af5154e6
kbbushman <k.bushman@yahoo.com>
2021-01-31 23:11:15 -0800

    Adds AppProvider with global styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AppProviders from "components/AppProviders";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AppProviders>
+    <App />
+  </AppProviders>, document.getElementById("root"));
