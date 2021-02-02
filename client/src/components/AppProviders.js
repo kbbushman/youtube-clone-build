22588bbf5eda0780ee149437711e3bbbf217f79c
kbbushman <k.bushman@yahoo.com>
2021-02-01 18:34:36 -0800

    Adds BrowserRouter from react-router-dom

--- client/src/components/AppProviders.js
+++ client/src/components/AppProviders.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/GlobalStyle";
 import { darkTheme } from "../styles/theme";
 
 function AppProviders({ children }) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </Router>
   );
 }
 
