e34d79248f50ef7000fb3e66761f5f515e00d346
kbbushman <k.bushman@yahoo.com>
2021-02-04 23:57:53 -0800

    Adds custom hook for closing sidebar on url location change

--- client/src/hooks/use-location-change.js
+++ client/src/hooks/use-location-change.js
@@ -1,3 +1,23 @@
-function usePrevious() {}
+import React from 'react';
+import { useLocation } from "react-router-dom";
 
-export function useLocationChange() {}
+function usePrevious(value) {
+  const ref = React.useRef();
+
+  React.useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
+export function useLocationChange(callback) {
+  const location = useLocation();
+  const prevLocation = usePrevious(location);
+
+  React.useEffect(() => {
+    if (prevLocation?.pathname !== location.pathname) {
+      callback();
+    }
+  }, [location, prevLocation, callback]);
+}
