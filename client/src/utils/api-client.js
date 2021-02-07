8b481411a7708a310ab9f0203d403bb8d3591333
kbbushman <k.bushman@yahoo.com>
2021-02-07 14:13:36 -0800

    Adds axios client config and client call to google-login in authenticate

--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -1,4 +1,23 @@
-export function authenticate() {}
+import axios from "axios";
+
+export const client = axios.create({
+  baseURL: "/api/v1",
+});
+
+export function authenticate(response) {
+  client({
+    method: "POST",
+    url: "/auth/google-login",
+    data: { idToken: response.tokenId },
+  })
+    .then((res) => {
+      console.log("Sign in Success: ", res);
+      window.location.assign(window.location.href);
+    })
+    .catch((err) => {
+      console.group("Sign in error: ", err.response);
+    });
+}
 
 export async function signoutUser() {}
 
