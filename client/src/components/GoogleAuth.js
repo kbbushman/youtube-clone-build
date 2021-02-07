429505a3025ca3051e6574cb55cc01a9eb8a1716
kbbushman <k.bushman@yahoo.com>
2021-02-07 14:14:24 -0800

    Adds GoogleLogin from react-google-login with config

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import Button from "../styles/Auth";
 import { SignInIcon } from "./Icons";
+import { authenticate } from "utils/api-client";
 
 function GoogleAuth() {
   return (
-    <Button tabIndex={0} type="button">
-      <span className="outer">
-        <span className="inner">
-          <SignInIcon />
-        </span>
-        sign in
-      </span>
-    </Button>
+    <GoogleLogin
+      clientId="941567674278-gjkg5f89mqo5up4tujq1fuompihjoe3j.apps.googleusercontent.com"
+      cookiePolicy="single_host_origin"
+      onSuccess={authenticate}
+      onFailure={authenticate}
+      render={(renderProps) => (
+        <Button
+          tabIndex={0}
+          type="button"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          <span className="outer">
+            <span className="inner">
+              <SignInIcon />
+            </span>
+            sign in
+          </span>
+        </Button>
+      )}
+    />
   );
 }
 
