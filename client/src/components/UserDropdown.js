b7962d8169f73324edf3de255f09691b7b5ca611
kbbushman <k.bushman@yahoo.com>
2021-02-07 15:21:53 -0800

    Updates user avatar, adds user channel link and signout

--- client/src/components/UserDropdown.js
+++ client/src/components/UserDropdown.js
@@ -1,26 +1,25 @@
-import { Menu, MenuButton, MenuItem, MenuList } from "@reach/menu-button";
-import "@reach/menu-button/styles.css";
 import React from "react";
+import "@reach/menu-button/styles.css";
+import { Menu, MenuButton, MenuItem, MenuList } from "@reach/menu-button";
+import { useHistory } from "react-router-dom";
 import Avatar from "../styles/Avatar";
+import { signoutUser } from "utils/api-client";
 import { ChannelIcon, SignoutIcon } from "./Icons";
 
 function UserDropdown({ user }) {
-  console.log(user);
+  const history = useHistory();
+
   return (
     <Menu>
       <MenuButton>
-        <Avatar
-          className="pointer"
-          src="https://dummyimage.com/100x100"
-          alt="username avatar"
-        />
+        <Avatar className="pointer" src={user.avatar} alt={user.username} />
       </MenuButton>
       <MenuList>
-        <MenuItem onSelect={() => null}>
+        <MenuItem onSelect={() => history.push(`/channel/${user.id}`)}>
           <ChannelIcon />
           <span>Your channel</span>
         </MenuItem>
-        <MenuItem onSelect={() => null}>
+        <MenuItem onSelect={signoutUser}>
           <SignoutIcon />
           <span>Sign out</span>
         </MenuItem>
