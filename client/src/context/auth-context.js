172a4ebb998675764f175ea2ffc5e91171250322
kbbushman <k.bushman@yahoo.com>
2021-02-07 14:33:25 -0800

    Adds AuthProvider and useAuth for user auth global context

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,3 +1,22 @@
-export function AuthProvider() {}
+import React from "react";
+import { client } from "utils/api-client";
 
-export function useAuth() {}
+const AuthContext = React.createContext(null);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    client.get("/auth/me").then((res) => setUser(res.data));
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = React.useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider component");
+  }
+  return context;
+}
