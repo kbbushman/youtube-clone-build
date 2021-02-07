e534b413e6f60c397dbbf5e3daf3be1ca2c5f30f
kbbushman <k.bushman@yahoo.com>
2021-02-07 14:41:06 -0800

    Adds AuthProvider for user context

--- client/src/components/AppProviders.js
+++ client/src/components/AppProviders.js
@@ -1,5 +1,6 @@
+import { AuthProvider } from "context/auth-context";
 import React from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/GlobalStyle";
 import { darkTheme } from "../styles/theme";
@@ -7,10 +8,12 @@ import { darkTheme } from "../styles/theme";
 function AppProviders({ children }) {
   return (
     <Router>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider theme={darkTheme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </AuthProvider>
     </Router>
   );
 }
