015d7d6f6dd3a1895add60f81c2ad452a77192a6
kbbushman <k.bushman@yahoo.com>
2021-02-07 15:20:59 -0800

    Updates nav icons/links for auth user

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
-import { useAuth } from "context/auth-context";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Wrapper from "../styles/Navbar";
 import GoogleAuth from "./GoogleAuth";
-import { AppsIcon, HamburgerIcon, LogoIcon, SettingsIcon } from "./Icons";
 import Search from "./Search";
 import UserDropdown from "./UserDropdown";
+import UploadVideo from "./UploadVideo";
+import { AppsIcon, HamburgerIcon, LogoIcon, SettingsIcon } from "./Icons";
+import { useAuth } from "context/auth-context";
 
 function Navbar({ toggleSidebarOpen }) {
   const user = useAuth();
@@ -32,12 +33,8 @@ function Navbar({ toggleSidebarOpen }) {
       <Search />
 
       <ul>
-        <li>
-          <AppsIcon />
-        </li>
-        <li>
-          <SettingsIcon />
-        </li>
+        <li>{user ? <UploadVideo /> : <AppsIcon />}</li>
+        <li>{user ? <AppsIcon /> : <SettingsIcon />}</li>
         <li> {user ? <UserDropdown user={user} /> : <GoogleAuth />}</li>
       </ul>
     </Wrapper>
