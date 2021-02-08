297fcb038dea53f7a02fdc6fbb05d2914009d040
kbbushman <k.bushman@yahoo.com>
2021-02-07 20:31:56 -0800

    Adds props for modal video upload status and preview

--- client/src/components/UploadVideoModal.js
+++ client/src/components/UploadVideoModal.js
@@ -4,33 +4,52 @@ import Wrapper from "../styles/UploadVideoModal";
 import { CloseIcon } from "./Icons";
 import VideoPlayer from "./VideoPlayer";
 
-function UploadVideoModal() {
+function UploadVideoModal({
+  previewVideo,
+  thumbnail,
+  url,
+  defaultTitle,
+  closeModal,
+}) {
   const [tab, setTab] = React.useState("PREVIEW");
+  const [title, setTitle] = React.useState(defaultTitle);
+  const [description, setDescription] = React.useState("");
+
+  function handleTab() {}
 
   return (
     <Wrapper>
       <div className="modal-content">
         <div className="modal-header">
           <div className="modal-header-left">
-            <CloseIcon />
-            <h3>Video Uploaded!</h3>
+            <CloseIcon onClick={closeModal} />
+            <h3>{url ? "Video Uploaded!" : "Uploading..."}</h3>
           </div>
-          <div style={{ display: "block" }}>
-            <Button>{tab === "PREVIEW" ? "Next" : "Upload"}</Button>
+          <div style={{ display: url ? "block" : "none" }}>
+            <Button onClick={handleTab}>
+              {tab === "PREVIEW" ? "Next" : "Upload"}
+            </Button>
           </div>
         </div>
 
         {tab === "PREVIEW" && (
           <div className="tab video-preview">
-            <VideoPlayer />
+            <VideoPlayer previewUrl={previewVideo} video={url} />
           </div>
         )}
 
         {tab === "FORM" && (
           <div className="tab video-form">
             <h2>Video Details</h2>
-            <input type="text" placeholder="Enter your video title" />
-            <textarea placeholder="Tell viewers about your video" />
+            <input
+              type="text"
+              placeholder="Enter your video title"
+              value={title}
+            />
+            <textarea
+              placeholder="Tell viewers about your video"
+              value={description}
+            />
           </div>
         )}
       </div>
