08ba29744acf63ff1f716ecf91434be162caf7d7
kbbushman <k.bushman@yahoo.com>
2021-02-07 22:34:12 -0800

    Updates vjsPlayer for watch page, adds addVideoView on player end

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,23 +1,35 @@
 import React from "react";
+import { addVideoView } from "utils/api-client";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
 function VideoPlayer({ previewUrl, video }) {
   const videoRef = React.useRef();
 
+  const { id, url, thumbnail } = video;
+
   React.useEffect(() => {
     const vjsPlayer = videojs(videoRef.current);
+
+    if (!previewUrl) {
+      vjsPlayer.poster(thumbnail);
+      vjsPlayer.src(url);
+    }
 
     if (previewUrl) {
       vjsPlayer.src({ type: "video/mp4", src: previewUrl });
     }
+
+    vjsPlayer.on("ended", () => {
+      addVideoView(id);
+    });
 
     return () => {
       if (vjsPlayer) {
         vjsPlayer.dispose();
       }
     };
-  }, [previewUrl]);
+  }, [id, thumbnail, url, previewUrl]);
 
   return (
     <div data-vjs-player>
