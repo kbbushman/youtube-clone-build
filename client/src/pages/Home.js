34a72aaf89f84193b9aad9595ee5f2b685ba5cf9
kbbushman <k.bushman@yahoo.com>
2021-02-07 18:16:36 -0800

    Adds home query for recommended videos

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,20 @@
+import ErrorMessage from "components/ErrorMessage";
 import React from "react";
+import { useQuery } from "react-query";
+import HomeSkeleton from "skeletons/HomeSkeleton";
+import { client } from "utils/api-client";
 import Wrapper from "../styles/Home";
 import VideoGrid from "../styles/VideoGrid";
 
 function Home() {
+  const { data: videos, isLoading, isError, error } = useQuery("Home", () =>
+    client.get("/videos").then((res) => res.data.videos)
+  );
+
+  if (isLoading) return <HomeSkeleton />;
+  if (isError) return <ErrorMessage error={error} />;
+
+  console.log(videos);
   return (
     <Wrapper>
       <VideoGrid>Recommended videos</VideoGrid>
