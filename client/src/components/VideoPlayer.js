052eb87f3e825540e075cfa1f3058df11c70e13c
kbbushman <k.bushman@yahoo.com>
2021-02-07 20:33:09 -0800

    Adds video.js player

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,11 +1,29 @@
 import React from "react";
+import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
-function VideoPlayer() {
+function VideoPlayer({ previewUrl, video }) {
+  const videoRef = React.useRef();
+
+  React.useEffect(() => {
+    const vjsPlayer = videojs(videoRef.current);
+
+    if (previewUrl) {
+      vjsPlayer.src({ type: "video/mp4", src: previewUrl });
+    }
+
+    return () => {
+      if (vjsPlayer) {
+        vjsPlayer.dispose();
+      }
+    };
+  }, [previewUrl]);
+
   return (
     <div data-vjs-player>
       <video
         controls
+        ref={videoRef}
         className="video-js vjs-fluid vjs-big-play-centered"
       ></video>
     </div>
