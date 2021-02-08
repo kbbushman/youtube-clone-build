198fbeb1a53492868798e9817bdf0a49004c7b4b
kbbushman <k.bushman@yahoo.com>
2021-02-07 19:07:36 -0800

    Adds file size check with snackbar message

--- client/src/components/UploadVideo.js
+++ client/src/components/UploadVideo.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { useSnackbar } from "react-simple-snackbar";
 import { uploadMedia } from "utils/upload-media";
 import { UploadIcon } from "./Icons";
 
 function UploadVideo() {
+  const [openSnackbar, closeSnackbar] = useSnackbar();
+
   async function handleUploadVideo(event) {
     const file = event.target.files[0];
 
     if (file) {
+      const fileSize = file.size / 1000000; // in MB
+
+      if (fileSize > 50) {
+        return openSnackbar("Video file should be less than 50MB");
+      }
+
       const url = await uploadMedia({
         type: "video",
         file,
