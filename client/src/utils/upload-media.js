8f18e9d62ae29e3ed400079d62ea9943bcb6c301
kbbushman <k.bushman@yahoo.com>
2021-02-07 18:51:29 -0800

    Adds uploadMedia with cloudinary config

--- client/src/utils/upload-media.js
+++ client/src/utils/upload-media.js
@@ -1,1 +1,13 @@
-export async function uploadMedia() {}
+import axios from "axios";
+
+export async function uploadMedia({ type, file, preset }) {
+  const formData = new FormData();
+  formData.append("upload_preset", preset);
+  formData.append("file", file);
+
+  const data = await axios
+    .post(`https://api.cloudinary.com/v1_1/htbtcfwbs/${type}/upload`, formData)
+    .then((res) => res.data);
+
+  return data.secure_url;
+}
