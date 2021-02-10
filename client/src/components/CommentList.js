d885db5d24c59e98ec00e58198a7aa981b2c42a1
kbbushman <k.bushman@yahoo.com>
2021-02-10 00:03:02 -0800

    Fixes return typo

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -4,7 +4,7 @@ import { formatCreatedAt } from "utils/date";
 import DeleteCommentDropdown from "./DeleteCommentDropdown";
 
 function CommentList({ comments }) {
-  retun comments.map((comment) => (
+  return comments.map((comment) => (
     <Comment key={comment.id} comment={comment} />
   ));
 }
@@ -13,14 +13,21 @@ function Comment({ comment }) {
   return (
     <div className="comment">
       <Link to={`/channel/${comment.user.id}`}>
-        <img src={comment.user.avatar} alt={`${comment.user.username} avatar`} />
+        <img
+          src={comment.user.avatar}
+          alt={`${comment.user.username} avatar`}
+        />
       </Link>
       <div className="comment-info" style={{ flex: "1 1 0" }}>
         <p className="secondary">
           <span>
-            <Link to={`/channel/${comment.user.id}`} className="user-channel">{comment.user.username}</Link>
+            <Link to={`/channel/${comment.user.id}`} className="user-channel">
+              {comment.user.username}
+            </Link>
           </span>
-          <span style={{ marginLeft: "0.6rem" }}>{formatCreatedAt(comment.createdAt)}</span>
+          <span style={{ marginLeft: "0.6rem" }}>
+            {formatCreatedAt(comment.createdAt)}
+          </span>
         </p>
         <p>{comment.text}</p>
       </div>
