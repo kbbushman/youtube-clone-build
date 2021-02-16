510284c4fe401deebcd44c8acf864d0d2e71ce22
kbbushman <k.bushman@yahoo.com>
2021-02-15 23:59:22 -0800

    Adds useAuthAction

--- client/src/hooks/use-auth-action.js
+++ client/src/hooks/use-auth-action.js
@@ -1,1 +1,22 @@
-export default function useAuthAction() {}
+import { useAuth } from "context/auth-context";
+import { useGoogleLogin } from "react-google-login";
+import { authenticate } from "utils/api-client";
+
+export default function useAuthAction() {
+  const user = useAuth();
+  const { signIn } = useGoogleLogin({
+    onSuccess: authenticate,
+    clientId:
+      "941567674278-gjkg5f89mqo5up4tujq1fuompihjoe3j.apps.googleusercontent.com",
+  });
+
+  function handleAuthAction(authAction, data) {
+    if (user) {
+      authAction(data);
+    } else {
+      signIn();
+    }
+  }
+
+  return handleAuthAction;
+}
