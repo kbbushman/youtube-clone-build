d7efdff91dcc7477b51683abe5d0fe44a06881f2
kbbushman <k.bushman@yahoo.com>
2021-02-21 23:43:41 -0800

    Adds channel suggestions if no subscriptions

--- client/src/components/ChannelSuggestions.js
+++ client/src/components/ChannelSuggestions.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useQuery } from "react-query";
+import SuggestionSkeleton from "skeletons/SuggestionSkeleton";
+import { client } from "utils/api-client";
 import Wrapper from "../styles/Trending";
+import ChannelInfo from "./ChannelInfo";
+import ErrorMessage from "./ErrorMessage";
 
 function ChannelSuggestions() {
+  const { data: channels, isLoading, isError, error, isSuccess } = useQuery('Channels', () => client.get('/users').then((res) => res.data.channels))
+
+  if (isLoading) return <SuggestionSkeleton />;
+  if (isError) return <ErrorMessage error={error} />
+
   return (
     <Wrapper>
       <h2>Suggestions For You</h2>
-      Channel Suggestions
+      {isSuccess && channels.map((channel) => (
+        <ChannelInfo key={channel.id} channel={channel} />
+      ))}
     </Wrapper>
   );
 }
