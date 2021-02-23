5536e8d69d564fff1871503290054aa827c9c686
kbbushman <k.bushman@yahoo.com>
2021-02-22 23:07:24 -0800

    Adds watch history

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,13 +1,23 @@
 // @ts-nocheck
+import ErrorMessage from "components/ErrorMessage";
+import TrendingCard from "components/TrendingCard";
+import { useAuth } from "context/auth-context";
 import React from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import TrendingSkeleton from "skeletons/TrendingSkeleton";
+import { client } from "utils/api-client";
 import { HistoryIcon } from "../components/Icons";
 import SignUpCard from "../components/SignUpCard";
 import Wrapper from "../styles/Trending";
 
 function History() {
-  const isAuth = false;
+  const user = useAuth();
+  const { data: videos, isLoading, isError, error, isSuccess } = useQuery("History", () => client.get('/users/history').then((res) => res.data.videos), {
+    enabled: user,
+  });
 
-  if (!isAuth) {
+  if (!user) {
     return (
       <SignUpCard
         icon={<HistoryIcon />}
@@ -17,10 +27,22 @@ function History() {
     );
   }
 
+  if (isLoading) return <TrendingSkeleton />;
+  if (isError) return <ErrorMessage error={error} />;
+
   return (
     <Wrapper noPad>
       <h2>History</h2>
-      Watched Videos
+      {isSuccess && !videos.length && (
+        <p className='secondary'>
+          videos that you have watched will show up here
+        </p>
+      )}
+      {isSuccess && videos.map((video) => (
+        <Link key={video.id} to={`/watch/${video.id}`}>
+          <TrendingCard video={video} />
+        </Link>
+      ))}
     </Wrapper>
   );
 }
