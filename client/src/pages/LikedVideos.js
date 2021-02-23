c908219945d65d870fbb83687d7d0a566b643ddf
kbbushman <k.bushman@yahoo.com>
2021-02-23 14:42:24 -0800

    Adds liked videos page

--- client/src/pages/LikedVideos.js
+++ client/src/pages/LikedVideos.js
@@ -1,12 +1,25 @@
+import ErrorMessage from "components/ErrorMessage";
+import TrendingCard from "components/TrendingCard";
+import { useAuth } from "context/auth-context";
 import React from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import TrendingSkeleton from "skeletons/TrendingSkeleton";
+import { client } from "utils/api-client";
 import { ChannelIcon } from "../components/Icons";
 import SignUpCard from "../components/SignUpCard";
 import Wrapper from "../styles/Trending";
 
 function LikedVideos() {
-  const isAuth = false;
+  const user = useAuth();
+  const { data: videos, isLoading, isError, isSuccess, error }  = useQuery("LikedVideos", () => client.get('/users/liked-videos').then((res) => res.data.videos), {
+    enabled: user,
+  });
 
-  if (!isAuth) {
+  if (isLoading) return <TrendingSkeleton />;
+  if (isError) return <ErrorMessage error={error} />
+
+  if (!user) {
     return (
       <SignUpCard
         icon={<ChannelIcon />}
@@ -19,8 +32,16 @@ function LikedVideos() {
   return (
     <Wrapper>
       <h2>Liked Videos</h2>
-      <p className="secondary">Videos that you have liked will show up here</p>
-      Liked Videos
+      {isSuccess && !videos.length && (
+        <p className='secondary'>
+          videos that you have liked will show up here
+        </p>
+      )}
+      {isSuccess && videos.map((video) => (
+        <Link key={video.id} to={`/watch/${video.id}`}>
+          <TrendingCard video={video} />
+        </Link>
+      ))}
     </Wrapper>
   );
 }
