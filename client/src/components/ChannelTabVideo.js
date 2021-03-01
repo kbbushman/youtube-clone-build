e92dbdeec84bc4eb10dc0eb315bcc6cb974e6ca2
kbbushman <k.bushman@yahoo.com>
2021-02-28 19:22:51 -0800

    Adds videos to channel tab

--- client/src/components/ChannelTabVideo.js
+++ client/src/components/ChannelTabVideo.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Wrapper from "../styles/ChannelTabVideo";
+import VideoCard from "./VideoCard";
 
-function ChannelTabVideo() {
-  return <Wrapper>Channel Videos</Wrapper>;
+function ChannelTabVideo({ videos }) {
+  if (!videos.length) {
+    return <p>This channel has not posted any videos yet</p>
+  }
+  return <Wrapper>{videos.length && videos.map((video) => <VideoCard key={video.id} video={video} noUsername hideAvatar />)}</Wrapper>;
 }
 
 export default ChannelTabVideo;
