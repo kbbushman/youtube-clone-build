907bbfeb914fafd7afa89f75b15b9ebec01244cf
kbbushman <k.bushman@yahoo.com>
2021-03-02 18:20:55 -0800

    Adds ErrorBoundary component from react-error-boundary

--- client/src/components/AppProviders.js
+++ client/src/components/AppProviders.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { ReactQueryConfigProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
+import { ErrorBoundary } from 'react-error-boundary';
 import GlobalStyle from "../styles/GlobalStyle";
 import { AuthProvider } from "context/auth-context";
 import { darkTheme } from "../styles/theme";
+import ErrorFallback from "./ErrorFallback";
 
 const config = {
   queries: {
@@ -21,19 +23,21 @@ const config = {
 
 function AppProviders({ children }) {
   return (
-    <ReactQueryConfigProvider config={config}>
-      <Router>
-        <AuthProvider>
-          <SnackbarProvider>
-            <ThemeProvider theme={darkTheme}>
-              <GlobalStyle />
-              <ReactQueryDevtools />
-              {children}
-            </ThemeProvider>
-          </SnackbarProvider>
-        </AuthProvider>
-      </Router>
-    </ReactQueryConfigProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ReactQueryConfigProvider config={config}>
+        <Router>
+          <AuthProvider>
+            <SnackbarProvider>
+              <ThemeProvider theme={darkTheme}>
+                <GlobalStyle />
+                <ReactQueryDevtools />
+                {children}
+              </ThemeProvider>
+            </SnackbarProvider>
+          </AuthProvider>
+        </Router>
+      </ReactQueryConfigProvider>
+    </ErrorBoundary>
   );
 }
 
